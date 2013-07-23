bb98800a8f5f98ca005bd1a4a4ddd97db75aa698
Ash Chan <ashchan1122@gmail.com>
2013-07-24 00:34:27 +0800

    Installation Pack Version v1.0

---
+++ README.md
@@ -0,0 +1,25 @@
+Section Z DayZ Install
+======================
+DayZ Version:
+----------------------
+**1.7.7.1**
+
+Installation:
+----------------------
+**Steps:**  
+1) Download ArmA 2 Combined Operations 1.60 - [http://thepiratebay.se/torrent/...repack_full%29_%28LAN%29_3xDVD5] (http://thepiratebay.se/torrent/7203731/ArmA_2_Combined_Operations_1.60_%28repack_full%29_%28LAN%29_3xDVD5)
+
+2) Download this installation pack and unzip to your game directory.
+
+3) Start `Section_Z_Easy_DayZ_Installer_v2.3.bat`, run #1 and #2 to install DayZ mod.
+
+4) Start `Section_Z_Game_Launcher_v3.3.bat` and input the corresponding map/mod code to launch the game.
+
+Author:
+----------------------
+Created by CE28N, Installation Pack Version 1.0 @ 29 June,2013
+
+Suggest Tools:
+----------------------
+ - DAEMON Tools
+ - WinRAR
---
+++ Section_Z_Easy_DayZ_Installer_v2.3.bat
@@ -0,0 +1,174 @@
+@echo off
+COLOR 0D
+set menu=%1
+
+echo #########################################################################
+echo #                                                                       #
+echo #            Welcome to Section Z easy DayZ installler v2.3.            #
+echo #     This little script will help you to install DayZ on your PC.      #
+echo #   If you have any trouble, please report on our RaidCall: 25597687.   #
+echo #                                                                       #
+echo #                                           Installer created by CE28N  #
+echo #########################################################################
+echo.
+
+:install
+echo.
+echo If you are ready to install, press any key to continue.
+pause
+cls
+goto menu
+
+:menu
+cls
+echo Welcome to Section Z easy DayZ installer menu. 
+echo.
+echo ArmA 2 settings
+echo ---------------
+echo 1 - Change your ArmA 2 and OA CD keys
+echo 2 - Install ArmA 2 Beta Patch 108074
+echo 3 - Download the latest ArmA 2 Beta patches
+echo.
+echo DayZ FAQ
+echo ---------------
+echo 4 - I can't change my CD-KEY, it says "Access Denied"?
+echo 5 - I can't enter the game because of "Bad CD-KEY given in setup"?
+echo 6 - I entered the server, but it says "Invalid CD-KEY"?
+echo 7 - My DayZ save in a server has been changed even i'm not online?
+echo 8 - I saw all the servers are with a red-cross, what's wrong?
+echo 9 - I can't install the game completely, the command prompt stops at 35%%
+echo.
+echo E - Exit this installer
+echo.
+set /P menu=Select a number or lettter: 
+if "%menu%"=="1" start dayz_install\arma2_keygen.exe & cls & goto cd_key
+if "%menu%"=="2" cls & goto beta
+if "%menu%"=="3" start http://www.arma2.com/beta-patch.php & cls & goto menu
+if "%menu%"=="4" cls & goto key_error_ac
+if "%menu%"=="5" cls & goto key_error_bad
+if "%menu%"=="6" cls & goto key_error_invalid
+if "%menu%"=="7" cls & goto key_error_save
+if "%menu%"=="8" cls & goto server_redcross
+if "%menu%"=="9" cls & goto error_35
+if "%menu%"=="e" exit
+if "%menu%"=="E" exit
+
+:cd_key
+if exist SimpleShutdownTimer.exe del /f/q SimpleShutdownTimer.exe >nul
+if exist "%commonprogramfiles(x86)%" (
+	FOR /F "tokens=2* delims=	 " %%A IN ('REG QUERY "HKLM\Software\Wow6432Node\Bohemia Interactive Studio\ArmA 2" /v KEY 2^>nul') DO SET arma2key=%%B
+	FOR /F "tokens=2* delims=	 " %%A IN ('REG QUERY "HKLM\Software\Wow6432Node\Bohemia Interactive Studio\ArmA 2 OA" /v KEY 2^>nul') DO SET arma2OAkey=%%B
+) else (
+	FOR /F "tokens=2* delims=	 " %%A IN ('REG QUERY "HKLM\Software\Bohemia Interactive Studio\ArmA 2" /v KEY 2^>nul') DO SET arma2key=%%B
+	FOR /F "tokens=2* delims=	 " %%A IN ('REG QUERY "HKLM\Software\Bohemia Interactive Studio\ArmA 2 OA" /v KEY 2^>nul') DO SET arma2OAkey=%%B
+)
+SET elegir_serial=99
+cls
+echo Applying a new CD-KEY to your game... 
+echo.
+if "%arma2key%"=="" (
+	echo There is no ArmA 2 key installed. You must install one.
+) else (
+	echo Current ArmA 2 key: %arma2key%
+)
+if "%arma2OAkey%"=="" (
+	echo There is no ArmA 2 OA key installed. You must install one.
+) else (
+	echo Current ArmA 2 : OA key: %arma2OAkey%
+)	
+echo.
+
+dayz_install\arma2_keygen.exe
+
+echo.
+echo Generate ArmA 2 and ArmA 2 : OA key with the keygen
+echo and copy the LOWER SERIALS to here WITHOUT SPACE!
+echo.
+echo.
+SET /P arma2key=Type 30 digits to use as ArmA 2 serial number: 
+SET /P arma2OAkey=Type 30 digits to use as ArmA 2 : OA serial number: 
+
+if exist "%commonprogramfiles(x86)%" (
+	if not "%arma2key%"=="" REG ADD "HKLM\Software\Wow6432Node\Bohemia Interactive Studio\ArmA 2" /f /v KEY /t REG_BINARY /d "%arma2key%"
+	if not "%arma2OAkey%"=="" REG ADD "HKLM\Software\Wow6432Node\Bohemia Interactive Studio\ArmA 2 OA" /f /v KEY /t REG_BINARY /d "%arma2OAkey%"
+) else (
+	if not "%arma2key%"=="" REG ADD "HKLM\Software\Bohemia Interactive Studio\ArmA 2" /v KEY /t REG_BINARY /d "%arma2key%"
+	if not "%arma2OAkey%"=="" REG ADD "HKLM\Software\Bohemia Interactive Studio\ArmA 2 OA" /v KEY /t REG_BINARY /d "%arma2OAkey%"
+)
+
+cls
+echo New key successfully installed.
+pause
+cls
+goto menu
+
+:beta
+if exist Expansion\beta\arma2oa.exe (
+	goto menu
+) else (
+	echo.
+	echo Installing ARMA2_OA_Build_108074...
+	dayz_install\ARMA2_OA_Build_108074.exe
+)
+cls
+goto menu
+
+:key_error_ac
+echo.
+echo If you can't change your CD-KEY because of "Access Denied",
+echo Please run the program with administrator.
+echo.
+pause
+cls & goto menu
+
+:key_error_bad
+echo.
+echo If you can't enter the game because of "Bad CD-KEY given in setup"
+echo Please run the program and change another CD-KEY to your game.
+echo.
+pause
+cls & goto menu
+
+:key_error_invalid
+echo.
+echo If you entered the server but it says "Invalid CD-KEY", 
+echo Please make sure your server filter has inputted "NO CD"/"NO-CD"/"NOCD" or kind of tags.
+echo Illegal copies of ArmA2 can NOT join the genuine server unless you bought the game.
+echo.
+pause
+cls & goto menu
+
+:key_error_save
+echo.
+echo If you found that your DayZ save in a server has been changed even you're not online,
+echo Which means you're using the same CD-KEY with others because it is illegal copy.
+echo You may change your key again, or buy the genuine version of the game.
+echo.
+pause
+cls & goto menu
+
+:server_redcross
+echo.
+echo If you saw all the servers are with a red-cross,
+echo Please make sure you're using the same Mods and version with the server.
+echo Also you can check your server filter to see if you did something wrong.
+echo.
+pause
+cls & goto menu
+
+:error_35
+echo.
+echo If you're facing the problem of install failure because the command prompt stops at 35%%,
+echo You need to input a command to set a variable and restart your computer to re-install.
+echo This program will input that command for you automatically, press any key to continue.
+echo.
+pause
+bcdedit /set IncreaseUserVA 2990
+cls
+echo.
+echo The command is done on your computer, press any key to reboot your computer now.
+echo Your computer is done with the settings, please re-install the game after the reboot.
+echo.
+pause
+shutdown /r
+cls & goto menu
